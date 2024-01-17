0bba1a35fb72dd7faad75b1e2a8198439e795f6e
Pema Yangzom <pema.yangzom@selise.ch>
2024-01-17 18:26:10 +0600

    ui

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['Your Font Name', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
