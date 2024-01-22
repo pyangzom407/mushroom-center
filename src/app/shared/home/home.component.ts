4e25c3cfb34e0c0e0e6550a8f38f1f27d92d5a51
Pema Yangzom <pema.yangzom@selise.ch>
2024-01-22 18:53:49 +0600

    ui

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -13,6 +13,48 @@ export class HomeComponent implements OnInit {
     '/assets/images/slide-m5.jpg',
 
   ];
+  service: any = [
+    { label: 'Free Shipping', hint: 'Free on order over $300', icon: 'local_shipping' },
+    { label: 'Security Payment', hint: '100% security payment', icon: 'credit_card' },
+    { label: '30 Day Return', hint: '30 day money guarantee', icon: 'currency_exchange' },
+    { label: '24/7 Support', hint: 'Support every time fast', icon: 'call' },
+  ];
+  product: any = [
+    { categories: 'All Products'},
+    { categories: 'Mushrooms'},
+    { categories: 'Vegetables'},
+    { categories: 'Fruits'},
+  ];
+  cards: any = [
+    {image:'assets/images/mushroom1.png', product_type: 'Mushrooms', title: 'Mushroom', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/onion.png', product_type: 'Vegetables', title: 'Onion', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/apple.png', product_type: 'Fruits', title: 'Apple', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/mushroom2.png', product_type: 'Mushroom', title: 'Mushroom', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/cabages.png', product_type: 'Vegetables', title: 'Cabbages', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/mushroom3.png', product_type: 'Mushrooms', title: 'Mushroom', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/tomato.png', product_type: 'Vegetables', title: 'Tomato', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/orange.png', product_type: 'Fruits', title: 'Orange', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',}
+
+  ]
+  ;
+  navbars: any = [
+    { icon: 'house', text: ' HOME'},
+    { icon: 'category', text: ' Services'},
+    { icon: 'shop', text: ' Shop'},
+    { icon: 'tty', text: ' Contact'},
+  ];
+  offer: any = [
+    { image: 'assets/images/tomato.png', product_status: 'Healthy Vegtables', offer: '20% off', bg_color: 'bg-primary'},
+    { image: 'assets/images/apple.png', product_status: 'Fresh Apple', offer: 'Free delivery', bg_color: 'bg_color'},
+    { image: 'assets/images/mushroom3.png', product_status: 'Fresh Apple', offer: '25% discount', bg_color: 'bg-light_red'}
+  ];
+  vegetables: any = [
+    {image:'assets/images/cabages.png', product_type: 'Vegetables', title: 'Cabbages', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/mushroom3.png', product_type: 'Mushrooms', title: 'Mushroom', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/tomato.png', product_type: 'Vegetables', title: 'Tomato', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/orange.png', product_type: 'Fruits', title: 'Orange', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+    {image:'assets/images/orange.png', product_type: 'Fruits', title: 'Orange', sub_text: 'This mushroom is 100% organic and its good for health', price: '100',},
+  ];
 
   constructor() { }
 
--- src/assets/config/variables.ts
+++ src/assets/config/variables.ts
@@ -2,5 +2,8 @@ module.exports = {
   primary: '#2DD4BF',
   rose_red: '#C85250',
   gray: '#808080',
-  light_gray: '#C5C5C5'
+  light_gray: '#C5C5C5',
+  bg_color: '#f4f6f8',
+  orange: '#FF8300',
+  light_red : '#FF7F7F'
 }
